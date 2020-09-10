a7d99c16d77f3b26c5c184805bac5b88f8ce7629
Scott <you@example.com>
2020-09-10 14:46:23 +0200

    poemprinting toegevoegd

---
+++ PYTB1L2PoemPrinting/gedicht.py
@@ -0,0 +1,7 @@
+vers1= "Mijn naam is Scott van den Broek. "
+vers2= "Ik lees of luister graag een boek. "
+vers3= "Ik woon in Purmerend met mijn ouders. "
+vers4= "Met school heb ik soms veel druk op mijn schouders. "
+vers5= "Mijn sport is basketbal. "
+vers6= "Als we gaan skieen, verblijven we in het dal. "
+print(vers1 + vers2 + vers3 + vers4 + vers5 + vers6)
