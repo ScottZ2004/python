05cbe46a78e6451a007df01365efbc2b6f2a5e28
Scott <you@example.com>
2020-09-15 10:20:01 +0200

    PLayerStatistics toegevoegd

--- PYTB1L2PlayerStatistics/player statistics.py
+++ PYTB1L2PlayerStatistics/player statistics.py
@@ -1,11 +1,21 @@
-name= "name: SkullCrusher "
-power= "power: 280W "
-plasma_ammo= "plasma ammo: 300P "
-jump= "jump: 10 "
-grenade= "number of grenades: 4 "
-gas_mask= "on "
-special_talent= "special talent: invincebility "
-speed= "speed: 80 "
-ultimate_charge= "ultimate charge: 78% "
-cammo= "cammo: true "
-print(name + power + plasma_ammo + jump + grenade + gas_mask + special_talent + speed + ultimate_charge + cammo)
+name = "SkullCrusher"
+Power = 280
+plasma_ammo = 300
+jump = 10 
+grenade =  4
+gas_mask = True 
+special_talent = "invincebility"
+speed =  80 
+ultimate_charge = 78
+cammo = True
+
+print("name:", name)
+print("power:", Power)
+print("plasma ammo:", plasma_ammo)
+print("jump:", jump)
+print("number of grenades:", grenade)
+print("gas mask:", gas_mask)
+print("special talent:", special_talent)
+print("speed:", speed)
+print("ultimate charge:", ultimate_charge, "%")
+print("cammo:",cammo)
