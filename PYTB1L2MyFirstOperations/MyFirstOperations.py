4ff6edfe207f15522365c7c7a8f8cd9dddf0f152
Scott <you@example.com>
2020-09-16 11:49:29 +0200

    MyFirstOperations toegevoegd

---
+++ PYTB1L2MyFirstOperations/MyFirstOperations.py
@@ -0,0 +1,33 @@
+#Welke berekeningen
+#toekennen = check
+#optellen + check
+#aftrekken - check
+#delen / check
+#restwaarde %
+#optellen bij waarde van variabele += check
+#delen door waarde van variabele /=
+
+getal1 = 12
+
+getal2 = 2
+
+print("12 * 2 = ", getal1 * getal2)
+
+getal3 = 15
+print("12 + 15 =", getal1 + getal3)
+
+print("15 - 2 =", getal3 - getal2)
+
+print("15 / 2 =", getal3 / getal2)
+
+getal4 = 25
+
+print("25 % 12 =", getal4 % getal1)
+
+getal2 += 24
+
+print("2 + 24 = ", getal2)
+
+getal2 /= 26
+
+print("26 / 26 = ", getal2)
