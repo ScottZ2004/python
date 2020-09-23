c31d1c24b71ccce311d6040e656b51e64e90565e
Scott <you@example.com>
2020-09-23 12:01:03 +0200

    Family ties toegevoegd

---
+++ PYTB1L2FamliyTies/family_ties.py
@@ -0,0 +1,14 @@
+naam = "Scott"
+leeftijd = 16
+woonplaats = "Purmerend"
+afstand = 16,6
+game1 = "Super mario galaxy 1 en 2"
+game2 = "Zelda"
+game3 = "Overwatch"
+sport = "basketbal"
+keren_trainen = 2
+
+print("Mijn naam is" ,naam ,", ik ben", leeftijd, "jaar oud en ik woon in", woonplaats)
+print("Ik woon", afstand, "kilometer van het mediacollege.")
+print("Mijn favoriete games zijn:", game1, ",", game2, "en", game3)
+print("Ik doe aan de sport", sport, ", ik train", keren_trainen, "keer per week en in het weekend het ik meestal een wedstijd")
