9ae23ac71cb4bf16ee7f2cbc2f4b943a5ef20727
Scott <you@example.com>
2020-09-23 11:25:20 +0200

    PYTB1L2InterestingTellMeMore toegevoegd

---
+++ PYTB1L2InterestingTellMeMore/stel_vragen.py
@@ -0,0 +1,21 @@
+print("Hallo!")
+print("Wat is jouw naam?")
+
+naam = input(">>>")
+
+print("Hey, " + naam)
+print("Hoe oud ben je eigenlijk?")
+
+leeftijd = input(">>>")
+
+print("En op welke datum ben je geboren?")
+
+datum = input(">>>")
+
+print("oke, nu de laatste vraag!")
+print("waar woon je?")
+
+woonplaats = input(">>>")
+
+print("Jouw naam is dus", naam , ", je bent", leeftijd , ", je bent geboren op", datum ,", en je woont in", woonplaats)
+
