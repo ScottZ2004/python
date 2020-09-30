b9bf792e3a618cc6d8039c089c1be999740f0696
Scott <you@example.com>
2020-09-30 12:27:47 +0200

    PYTB1L2SliceWise toegevoegd

---
+++ PYTB1L2SliceWise/PYTB1L2SliceWise.py
@@ -0,0 +1,21 @@
+tekst = 'De Python lessen worden gegeven voor Erik, Erwin en ook door Hidde'
+print(tekst[37:41])
+print(tekst[43:49])
+print(tekst[61:66])
+
+tekst2 = "SD vakken zijn Python, UXD, Frontend development en Backend development en nog veel meer ..."
+print(tekst2[15:21])
+print(tekst2[23:26])
+print(tekst2[28:48])
+print(tekst2[52:71])
+
+tekst3 = "Wat is hier het laatste woord?"
+lengte_laatste = len(tekst3)
+lengte_eerste = lengte_laatste - 6
+print(lengte_eerste)
+print(lengte_laatste)
+print(tekst3[lengte_eerste:lengte_laatste])
+
+tekst4 = "Het www is ontwikkeld vanaf 1989 door Tim Berners-Lee"
+print(tekst4[5:8])
+print(tekst4[29:33])
--- PYTB1L2StutterSimulator/stutter_simulator.py
+++ PYTB1L2StutterSimulator/stutter_simulator.py
@@ -7,7 +7,8 @@ while running == True:
     for x in userlist:
         if len(x) > 2:
             print(x[0:2]+"...", x[0:2]+"..." ,x)
-            
         else:
             print(x)
-        
+
+    
+    
