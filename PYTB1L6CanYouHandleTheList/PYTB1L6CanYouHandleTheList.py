3889889e4e1ffcbe5f5174b002dd40286e43836e
Scott <you@example.com>
2020-10-23 11:56:03 +0200

    PYTB1L6CanYouHandleTheList toegevoegd

---
+++ PYTB1L6CanYouHandleTheList/PYTB1L6CanYouHandleTheList.py
@@ -0,0 +1,21 @@
+#lijstje gemaakt
+lijstje = ["Scott", True, 69]
+
+print(lijstje)
+#"Ik" toegevoegd aan het lijstje
+lijstje.append("Ik")
+
+print(lijstje)
+#True en 69 verwijderd
+lijstje.remove(True)
+lijstje.remove(69)
+
+print(lijstje)
+#Ben toegevoegd
+lijstje.append("Ben")
+#lijstje gesorteerd
+lijstje.sort()
+#vraagteken toegevoegd
+lijstje.append("?")
+
+print(lijstje)
